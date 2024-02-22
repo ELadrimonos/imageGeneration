f421bbfc628704f98344746552c2b23523ea8c7a
Adrián Primo <adrianprimob@gmail.com>
2024-02-22 03:20:28 +0100

    Terminado

--- README.md
+++ README.md
@@ -3,3 +3,6 @@
 - pip install Pillow==9.4.0
 - pip install openai==1.7.2
 - pip install gradio==4.14.0
+- pip install python-dotenv
+
+Agregar .env con OPENAI_KEY
--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-from openai import OpenAI
 import os
+from PIL import Image
 from dotenv import load_dotenv
-
+from openai import OpenAI
+import gradio as gr
+import requests
+import numpy as np
 
 load_dotenv()
 
@@ -9,13 +12,22 @@ print(os.environ['OPENAI_KEY'])
 
 client = OpenAI(api_key=os.environ['OPENAI_KEY'])
 
-response = client.images.generate(
-    model='dall-e-2',
-    prompt='langosta',
-    size='1024x1024',
-    quality='standard',
-    n=1
-)
 
-image_url = response.data[0].url
-print(image_url)
+def generate_image(image_prompt):
+    response = client.images.generate(
+        model='dall-e-2',
+        prompt=image_prompt,
+        size='1024x1024',
+        quality='standard',
+        n=1
+    )
+    image_url = response.data[0].url
+    image = Image.open(requests.get(image_url, stream=True).raw)
+    return np.array(image)
+
+
+gr_text_input = gr.Textbox(label="Prompt Dall-e-2", placeholder="Introduce un prompt")
+gr_image_output = gr.Image(type="numpy",label="Imagen generada por Dall-e")
+
+demo = gr.Interface(fn=generate_image, inputs=gr_text_input, outputs=gr_image_output)
+demo.launch()
